51200c582bb252802123f2c7e6564c8de54002b0
Yeomdonghwan <101257697+Yeomdonghwan@users.noreply.github.com>
2023-02-08 11:57:27 +0900

    Update main.js

--- 채팅/main.js
+++ 채팅/main.js
@@ -17,7 +17,7 @@
         $('.chat-content').scrollTop($('.chat-content').prop('scrollHeight'));
 
         const configuration = new Configuration({
-            apiKey: ${secret.SECRET_KEY},
+            apiKey: ${{secret.SECRET_KEY}},
         });
         const openai = new OpenAIApi(configuration);
 
